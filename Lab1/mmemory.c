7eb043867e082dfaa336e4847f27b7271aac90d3
PlagaMedicum <katoshrodingera@protonmail.ch>
2019-09-30 13:20:19 +0300

    Refactored structures and everything.

--- Lab1/mmemory.c
+++ Lab1/mmemory.c
@@ -2,67 +2,84 @@
 #include <unistd.h>
 #include <stdio.h>  // TODO: remove it after debug
 
-// segm_end returns VA of last allocated block of the segment x.
-#define segm_end(x) (x->va + x->l)
-
-// segment_pa returns physical address of the segment x in mmem.
-#define segment_pa(x) (mmem.b + (size_t)x->va * sizeof(VA))
-
-// ocup_space returns occupied space in memory mem.
-#define ocup_space(mem)((size_t)mem.te->va + mem.te->l)
-
-// free_spaceof returns amount of free space in the memory mem.
-#define free_space(mem) (mem.s - ocup_space(mmem))
-
-// ST is a stack that represents segments table.
+// ST is a queue that represents segments table.
 typedef struct ST
 {
-    size_t i;       // Segment index.
-	VA va;          // Segment VA. 
-    size_t l;       // Length of the segment.
-	struct ST* p;   // Physical address of prev segment's VA.
+	VA va;          // Segment VA.
+	struct ST* n;   // Physical address of next segment's VA.
 } ST;
 
 // MEMORY represents virtual address space.
 typedef struct
 {
-	VA b;		    // Points to the first block of memory.
-	size_t s;	    // Number of blocks in memory.
-    ST* te;         // Points to the ending of segments table stack.
+	size_t pa;		// Physical address of first block.
+	size_t sz;	    // Number of blocks in memory.
+    ST* fs;         // Points to the first segment in the table.
+    ST* ls;         // Points to the last segment in the table.
 } MEMORY;
 
 static MEMORY mmem; // Virtual address space instance.
 
-// rqmem allocates sz bytes of memory for provided pointer.
-size_t rqmem (size_t sz)
+// s_pa returns physical address of the segment x in mmem.
+#define s_pa(x) (mmem.pa + (size_t)x->va)
+
+// upd_ls updates last segment of the table.
+void upd_ls()
 {
-    size_t reqmem = sbrk(sz);
-    if (reqmem == -1)
+    ST* s = mmem.ls;
+    if(s->n == NULL)
+    {
+        return;
+    }
+    while(s->n->n != NULL)
+    {
+        s = s->n;
+    }
+}
+
+// s_len returns VA of last allocated block of the segment s.
+size_t s_len (const ST* s)
+{
+    if(s->n == NULL)
+    {
+        return -1;
+    }
+    return (size_t)s->n->va - 1;
+}
+
+// free_spaceof returns amount of free space in the memory.
+#define free_space() (mmem.sz - s_len(mmem.ls))
+
+// rqmem allocates sz bytes of memory for provided pointer.
+size_t rqmem (const size_t sz)
+{
+    size_t addr = sbrk(sz);
+    if (addr == -1)
     {
         return -1;
     }
 
-    return reqmem;
+    return addr;
 }
 
 // ptrs returns physical adress of the segment
 // which ptr belongs to.
-ST* ptrs (VA ptr)
+ST* ptrs (const VA ptr)
 {
-    if ((ptr < 0) || (ptr > (VA)segm_end(mmem.te)))
+    if ((ptr < 0) || (ptr > (VA)s_len(mmem.ls)))
     {
         return NULL;
     }
 
-    ST* segm = mmem.te;
-    while (segm->p != NULL)
+    ST* s = mmem.fs;
+    while (s->n != NULL)
     {
-        if (ptr > (VA)segm->va)
+        if (ptr > (VA)s->va)
         {
-            return segm;
+            return s;
         }
 
-        segm = segm->p;
+        s = s->n;
     }
 
     return NULL;
@@ -70,38 +87,32 @@ ST* ptrs (VA ptr)
 
 int _malloc (VA* ptr, size_t szBlock)
 {
-	if (szBlock > free_space(mmem))
+	if (szBlock > free_space())
 	{
 		return -2;
 	}
 
-    ST* prev = mmem.te;
-
-    size_t addr = rqmem(sizeof(ST*));
-    if (addr == -1)
-    {
-        return 1;
-    }
-    mmem.te = (ST*)addr;
-
-    addr = rqmem(szBlock * sizeof(VA));
+    size_t addr = rqmem(szBlock);
     if (addr == -1)
     {
         return 1;
     }
     *ptr = (VA)addr;
-    
-    mmem.te->i = prev->i + 1;
-    mmem.te->va = prev->va + prev->l;
-    mmem.te->l = szBlock;
-    mmem.te->p = prev;
+
+    ST* s = mmem.ls;
+    if(s->n != NULL)
+    {
+        s = s->n;
+    }
+    s->n = (ST*)rqmem(sizeof(ST));
+    s->n->va = (VA)(szBlock + 1);
 
     return 0;
 }
 
 int _free (VA ptr)
 {
-    if ((ptr < 0) || (ptr > (VA)segm_end(mmem.te)))
+    if ((ptr < 0) || (ptr > (VA)s_len(mmem.ls)))
     {
         return -1;
     }
@@ -112,41 +123,41 @@ int _free (VA ptr)
         return 1;
     }
 
-    // TODO: Segment shifting and freeing
-    
-    for(size_t el = (size_t)mmem.b + (size_t)s->va + s->l; el < ocup_space(mmem); el++)
+    size_t shift;
+    while(s->n != NULL)
     {
-        // need to shift va's
-        *((VA)el - s->l) = *((VA)el);
-        *((VA)el) = 0;
+        shift = (size_t)s->n->va - (size_t)s->va;
+        s = s->n;
+        for(size_t el = mmem.pa + (size_t)s->va; el < s_len(s); el++)
+        {
+            *((VA)(el - shift)) = *((VA)el);
+            *((VA)el) = 0;
+        }
     }
-
-    ST* next;
-    if(mmem.te->i != s->i)
 
     return 0;
 }
 
 int _read (VA ptr, void* pBuffer, size_t szBuffer)
 {
-	if ((ptr < 0) || (ptr > (VA)segm_end(mmem.te)))
+	if ((ptr < 0) || (ptr > (VA)s_len(mmem.ls)))
 	{
 		return -1;
 	}
     
-    ST* segm = ptrs(ptr);
-    if (szBuffer > segm->l)
+    ST* s = ptrs(ptr);
+    if (szBuffer > s_len(s))
     {
         return -2;
     }
-    if (segm == NULL)
+    if (s == NULL)
     {
         return 1;
     }
 
     for (int i = 0; i < szBuffer; i++)
     {
-        *((VA*)(pBuffer + i)) = *((VA*)(segment_pa(segm) + i));
+        *((VA*)(pBuffer + i)) = *((VA*)(s_pa(s) + i));
     }
 
     return 0;
@@ -154,24 +165,24 @@ int _read (VA ptr, void* pBuffer, size_t szBuffer)
 
 int _write (VA ptr, void* pBuffer, size_t szBuffer)
 {
-	if ((ptr < 0) || (ptr > (VA)segm_end(mmem.te)))
+	if ((ptr < 0) || (ptr > (VA)s_len(mmem.ls)))
     {
 		return -1;
 	}
 
-    ST* segm = ptrs(ptr);
-    if (szBuffer > segm->l)
+    ST* s = ptrs(ptr);
+    if (szBuffer > s_len(s))
     {
         return -2;
     }
-    if (segm == NULL)
+    if (s == NULL)
     {
         return 1;
     }
 
     for (int i = 0; i < szBuffer; i++)
     {
-        *((VA*)(segment_pa(segm) + i)) = *((VA*)(pBuffer + i));
+        *((VA*)(s_pa(s) + i)) = *((VA*)(pBuffer + i));
     }
 
     return 0;
@@ -189,13 +200,11 @@ int s_init (int n, int szPage)
     {
         return 1;
     }
-    mmem.b = (VA)addr;
+    mmem.pa = addr;
     
-    mmem.te = (ST*)rqmem(sizeof(ST*));
-    mmem.te->i = 0;
-    mmem.te->va = 0;
-    mmem.te->l = 0;
-    mmem.s = n * szPage;
+    mmem.fs = (ST*)rqmem(sizeof(ST*));
+    mmem.fs->va = 0;
+    mmem.sz = n * szPage + 1;
 	
     return 0;
 }
